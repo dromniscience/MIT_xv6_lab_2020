40beb1774d5866ff55f0ccc5973225d184b1d944
dingrui <dromniscience@gmail.com>
2020-10-28 08:52:55 -0700

    change the position of printf when tracing in case the index is out of range

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -168,14 +168,13 @@ syscall(void)
   num = p->trapframe->a7;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
     p->trapframe->a0 = syscalls[num]();
+    
+  	if(p->traced & (1 << num))
+  		printf("%d: syscall %s -> %d\n",
+  					p->pid, sysname[num], p->trapframe->a0);
   } else {
     printf("%d %s: unknown sys call %d\n",
             p->pid, p->name, num);
     p->trapframe->a0 = -1;
   }
-  
-  // p = myproc();
-  if(p->traced & (1 << num))
-  		printf("%d: syscall %s -> %d\n",
-  					p->pid, sysname[num], p->trapframe->a0);
 }
